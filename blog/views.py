9f2c1a334b9a97fb966ce9722de81bf4c7fa3794
Tysiachnyk <tloslvmind@gmail.com>
2024-08-01 16:19:27 +0300

    added sharing of posts by email

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
+from .forms import EmailPostForm
 
 def post_list(request):
     posts = Post.published.all()
@@ -20,3 +22,26 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
                              publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    # Извлечь пост по его идентификатору id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}"
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
